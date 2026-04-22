e88a62e4ed4d74a1817d3b5f5218e1f4f1aa5f4d
muhhazzisaputra <hazzihazzihazzi@gmail.com>
2026-04-22 21:09:25 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Aplikasi Laravel')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 font-sans antialiased">
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-5xl font-bold text-blue-600 underline">
+            Hello Laravel 12 with Tailwind CSS!
+        </h1>
+    </div>
+@endsection
